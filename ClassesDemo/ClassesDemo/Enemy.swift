9b85152472f252f392a1a1a393a8decd64591738
Guilherme Pardini <guipardini@brq.com>
2022-06-17 11:08:47 -0300

    Classes Demo

---
+++ ClassesDemo/ClassesDemo/Enemy.swift
@@ -0,0 +1,13 @@
+
+class Enemy {
+    var health = 100
+    var attackStregth = 10
+    
+    func move(){
+        print("Walk forwards.")
+    }
+    
+    func attack(){
+        print("Land a hit, does \(attackStregth) damage.")
+    }
+}
---
+++ ClassesDemo/ClassesDemo/main.swift
@@ -0,0 +1,4 @@
+
+let skeleton = Enemy()
+let dragon = Enemy()
+
