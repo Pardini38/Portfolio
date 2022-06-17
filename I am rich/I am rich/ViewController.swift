c1aac0c2cd9ffa5f0d7dad0f6b3506243d484e1e
Guilherme Pardini <guipardini@brq.com>
2022-06-17 11:18:16 -0300

    I am Rich

---
+++ I am rich/I am rich/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by User on 18/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+ 
+}
